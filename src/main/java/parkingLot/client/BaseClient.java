3da74a2da36fe4fbc04651e1277f692aa1bb2eb6
nisarga-pal <83667442+nisarga-pal@users.noreply.github.com>
2021-05-06 09:02:31 +0530

    Update BaseClient.java

--- src/main/java/parkingLot/client/BaseClient.java
+++ src/main/java/parkingLot/client/BaseClient.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 public abstract class BaseClient {
 	Scanner sc=new Scanner(System.in);
 	
-    public abstract void SetSlot() throws SQLException;{}
-    public abstract void ToRemoveFromSlot();
-    public abstract void SearchSlotByRegNumber();
-    public abstract void SearchCarByColor();
-    public abstract void searchSlotByColor();
+	public String color(){
+        	System.out.println("Enter Car Color : ");
+        	String color = sc.nextLine();
+        	return color;
+        	return sc.nextLine();
+        }
+	public String registrationNumber(){
+        	System.out.println("Enter Car Registration Number : ");
+        	String registrationNumber = sc.nextLine();
+        	return registrationNumber;
+        	return sc.nextLine();
+    	}
+        public abstract void SetSlot() throws SQLException;{}
+        public abstract void ToRemoveFromSlot();
+        public abstract void SearchSlotByRegNumber();
+        public abstract void SearchCarByColor();
+        public abstract void searchSlotByColor();
 }
